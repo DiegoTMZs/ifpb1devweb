b4f8d6e497daba6d7d637765931025a9a90dfe53
DiegoTMZs <dev.diegotomaz@gmail.com>
2025-04-28 16:00:31 -0300

    first commit

---
+++ servidor.py
@@ -0,0 +1,27 @@
+from flask import *
+
+#instanciador do servidor
+app = Flask(__name__)
+
+@app.route("/")
+def pagina_principal():
+    return render_template("index.html")
+
+@app.route("/sugestao", methods=["POST", "GET"])
+def receber_sugestao():
+    #METODO GET
+    sugestao = request.values.get("sugestao")
+
+    #METODO POST
+    sugestao = request.form.get("sugestao")
+
+    print(sugestao)
+    return render_template("index.html")
+
+@app.route("/informacoes_usuario", methods=["POST"])
+def receber_informacoes_usuario():
+    nome_usuario = request.form.get("nome_usuario")
+    peso_usuario = request.form.get("peso_usuario")
+    idade_usuario = request.form.get("idade_usuario")
+    return render_template("index.html")
+app.run()
